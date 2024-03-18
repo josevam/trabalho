5673a5c183acbd864bbc4a025d51b7a3eb25a3bc
josevam <154993791+josevam@users.noreply.github.com>
2024-03-18 20:41:10 -0300

    Add files via upload

---
+++ atividadeCFE - inicial para atividade/js/jquery-script.js
@@ -0,0 +1,14 @@
+//efeito de esconder formulário de cadastro
+
+$(document).ready(function(){
+
+    $("#botao-cadastrar").click(function(){
+
+        $("#form-cadastrar").slideToggle("slow");
+        $("#section-login").slideTogle("slow");
+        $("#botao-cadastrar").hide();
+
+    });
+
+
+});
